7c08a12dbeaedcd40021e42b3090747417365630
Vinícius Alves de Sousa <vinicius.sousa@aditec.local@C-DDSC-4593.aditec.local>
2025-10-15 16:54:53 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd"
+
+function App() {
+
+  return (  
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"> 
+        <button className="bg-red-400">Click</button>
+        <Button>Button antd</Button>
+      </div>
+  )
+}
+
+export default App
