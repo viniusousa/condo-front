b53007e3564dfe04bc552e33377eb85548368102
˜viniusousa˜ <viniciussmit@gmail.com>
2025-10-16 10:06:28 -0300

    add rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "antd"
-
 function App() {
 
   return (  
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"> 
-        <button className="bg-red-400">Click</button>
-        <Button>Button antd</Button>
-      </div>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100"> 
+        <button className="bg-red-400">Click to login</button>
+      </main>
   )
 }
 
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav className="w-full flex justify-between items-center bg-green-400 text-white font-bold">
+            <h1>Logo</h1>
+            <h1>Navbar</h1>
+        </nav>
+    )
+}
